0adc2ae2fc821b2f011f0972a9d09a01a8030fc4
sangwonsheep <swonyy1709@naver.com>
2024-07-13 11:08:02 +0900

    간단한 게시판 프로젝트
    뷰는 구현하지 않음.
    mysql을 이용한 ERD 설계.
    JPA 연관관계 설정.
    게시판, 게시글, 댓글 구현
    페이징 구현

---
+++ 09.simple-board/src/main/java/com/example/simple_board/board/model/BoardRequest.java
@@ -0,0 +1,20 @@
+package com.example.simple_board.board.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BoardRequest {
+
+    @NotBlank
+    private String boardName;
+}
